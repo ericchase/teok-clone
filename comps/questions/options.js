fc5ed395c3107481719a4487183c77b0e3913a34
ericchase <>
2022-05-20 00:06:30 -0500

    refactor data

--- comps/questions/options.js
+++ comps/questions/options.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
-import { ChangeAnswers } from '../../pages/data/que_content';
+import { ChangeAnswers } from '../../data/que_content';
 import { useState } from 'react';
 // import styles from '../styles/question.module.css'
 //push
